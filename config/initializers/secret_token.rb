4f1db4ac7eedf4d7827141a5aeecc3f47e8bffb7
maas <maasgultekin@gmail.com>
2013-08-12 23:52:57 +0300

    first refinery

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hospital::Application.config.secret_token = '6706689c0701921e6009c1d6b91000ce28c97befc11bccccee4ef6a95c083ea59d2212cda1b77ebb3380b5751cacdb7d4a469557d2fc59e992ec2579533c12a6'
